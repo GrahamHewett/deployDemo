226f971dd81d1a1f41d5f9319181ede0b4579b3d
Graham Hewett <hewettgraham@gmail.com>
2019-10-31 08:53:06 +0000

    add 6 movie covers and style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import avatar from "./images/avatar.jpg";
+import elem5 from "./images/elem5.jpg";
+import drStrange from "./images/drStrange.jpg";
+import gwh from "./images/gwh.jpg";
+import matrix from "./images/matrix.jpg";
+import oa from "./images/oa.jpg";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="movieCoverRow">
+        <figure className="movie">
+          <img src={matrix} alt="matrix movie cover" />
+        </figure>
+        <figure className="movie">
+          <img src={oa} alt="The OA TV show cover" />
+        </figure>
+        <figure className="movie">
+          <img src={gwh} alt="Good Will Hunting movie cover" />
+        </figure>
+      </section>
+      <section className="movieCoverRow">
+        <figure className="movie">
+          <img src={avatar} alt="Avatar movie cover" />
+        </figure>
+        <figure className="movie">
+          <img src={elem5} alt="The Fifth Element movie cover" />
+        </figure>
+        <figure className="movie">
+          <img src={drStrange} alt="Dr Strange movie cover" />
+        </figure>
+      </section>
+    </div>
+  );
 }
 
 export default App;
